7624f2eeed5d8982e08787b979ef6a8ad827c258
marcelo-py <marcelo.araujo.999s@gmail.com>
2023-11-12 18:17:08 -0300

    registrando no admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Palestrante, Evento, DatasParaEvento, Atividade
+from .models import CustomUser, Palestrante, Evento, DatasParaEvento, Atividade, AtividadeAlunos
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -22,9 +22,13 @@ class AtividadeAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', )
 
 
+class AtividadeAlunosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', )
+
 
 admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Palestrante, PalestranteAdmin)
 admin.site.register(Evento, EventoAdmin)
 admin.site.register(DatasParaEvento, DatasParaEventoAdmin)
 admin.site.register(Atividade, AtividadeAdmin)
+admin.site.register(AtividadeAlunos, AtividadeAlunosAdmin)
